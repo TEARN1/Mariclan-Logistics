a227434677ac137f3e6722e6bd723c1b59aeb777
Asemahle Nkwali <99318798+TEARN1@users.noreply.github.com>
2023-09-05 04:57:35 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,64 @@
+let slogan = document.getElementsByClassName("slo|gan");
+
+window.addEventListener("scroll", function() {
+    let value = window.screenY;
+    slogan.style.left = value + 1 + 'px';
+
+})
+
+function a() {
+ var a = document.getElementsByClassName('a');
+ var more = document.getElementsByClassName('more');
+
+ if(dot.style.display == "none") {
+    
+ }
+}
+
+
+function b() {
+    
+}
+function c() {
+
+}
+
+
+function d() {
+
+}
+
+
+function e() {
+
+}
+
+
+function f() {
+
+}
+
+function g() {
+
+}
+
+
+
+function h() {
+
+}
+
+
+
+function i() {
+
+}
+
+
+
+function j() {
+
+}
+
+
+
